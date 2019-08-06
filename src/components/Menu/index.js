c497bae2595d76de412acd2b6ea4f15c3567ae7a
Phoenix214 <phoenix214@protonmail.com>
2019-08-05 22:46:15 -0400

    fixed menu items

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -11,20 +11,20 @@ const Menu = props => {
   return (
     <div className="menu-container">
       <div className="menu">
-        <a className="menu-item" href='/#00' onClick={props.onMenuClose}>Home</a>
-        <a className="menu-item" href='/#01' onClick={props.onMenuClose}>Work</a>
         <Link className="menu-item" to='/aboutus#00' onClick={props.onMenuClose}>About</Link>
         <Link className="menu-item" to='/services#00' onClick={props.onMenuClose}>Services</Link>
-        {/* <Link className="menu-item" to='/blog' onClick={props.onMenuClose}>Blog</Link> */}
+        <Link className="menu-item" to='/team' onClick={props.onMenuClose}>Meet the team</Link>
+        <Link className="menu-item" to='/careers' onClick={props.onMenuClose}>Careers</Link>
+        <Link className="menu-item" to='/privacy-policy' onClick={props.onMenuClose}>Privacy Policy</Link>
         <div className="menu-contact-section">
-          <p><a href='tel:6169149356'>(616) 914-9356</a></p>
-          <p><a href='mailto:hello@pixoulinc.com'>hello@pixoulinc.com</a></p>
+          <p><a href='tel:16306494473'>+1 (630) 649-4473</a></p>
+          <p><a href='mailto:hello@pixoulinc.com'>hello@stratus-digital.com</a></p>
         </div>
         <a className="menu-contact-button" href="#Contact" onClick={props.onMenuClose}>
           <div>Contact Us</div>
         </a>
         <div className="menu-link-section">
-          <div>&copy; 2019 Pixoul, Inc. All rights reserved.</div>
+          <div>&copy; 2019 Stratus Digital, Inc. All rights reserved.</div>
           <Link className="small-menu-item" to='/privacy-policy'>Privacy Policy</Link> | <Link className="small-menu-item" to='/tos'>Terms of Service</Link>
         </div>
       </div>
